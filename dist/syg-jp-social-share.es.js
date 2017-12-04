09a607f122f4b0cb1b8ea6986095803a51257419
hiroshi fukuda <dada@sygnas.jp>
2017-12-04 12:21:00 +0800

    1.1.0 : ES5形式にした

---
+++ dist/syg-jp-social-share.es.js
@@ -0,0 +1,68 @@
+/**
+ * check media query
+ *
+ * @author   Hiroshi Fukuda <info.sygnas@gmail.com>
+ * @license  MIT
+ */
+
+var syg_mediaquery = {
+
+    /**
+     * media list
+     */
+    _medias: {
+        large: 'screen and (min-width:801px)',
+        tablet: 'screen and (min-width:601px) and (max-width:800px)',
+        mobile: 'screen and (max-width:600px)'
+    },
+
+    /**
+     * set media query
+     * @param name {String} media name
+     * @param value {String} media query string
+     */
+    set_media: function set_media(name, value) {
+        syg_mediaquery._medias[name] = value;
+    },
+
+
+    /**
+     * reset media query
+     * @param medias {Object} media query list
+     */
+    reset_medias: function reset_medias(medias) {
+        syg_mediaquery._medias = medias;
+    },
+
+
+    /**
+     * check target media
+     * @param name {String} media name
+     * @return {Bool} is hit target media
+     */
+    is_media: function is_media(name) {
+        var media = syg_mediaquery._medias[name];
+        return window.matchMedia(media).matches;
+    },
+
+
+    /**
+     * get hit media list
+     * @return {Array} hit media list
+     */
+    get_hit_medias: function get_hit_medias() {
+        var medias = syg_mediaquery._medias;
+        var hitlist = [];
+
+        Object.keys(medias).forEach(function (name) {
+            if (syg_mediaquery.is_media(name)) {
+                hitlist.push(name);
+            }
+        });
+
+        return hitlist;
+    }
+};
+
+export default syg_mediaquery;
+//# sourceMappingURL=syg-jp-social-share.es.js.map
